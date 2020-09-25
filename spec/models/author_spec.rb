60cffda94e247d40a18c5a78b04a0ca238fe8064
Hiroki Oshima <geet1876@gmail.com>
2020-09-25 14:34:17 +0900

    Added reproduce spec

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Author, type: :model do
   let!(:author) { create(:author) }
   let!(:book) { create(:book, author: author) }
 
-  it '' do
-    expect(author.valid?).to eq true
+  it 'アソシエーションが引ける' do
+    expect(author.books.first).to eq book
   end
 end
